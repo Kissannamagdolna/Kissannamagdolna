759f20c85a522644fce55ab3fcbaf7ad313b81e5
Kissannamagdolna <kiss.anna.magdolnaa@gmail.com>
2024-10-11 09:15:28 +0200

    initialization

---
+++ app.js
@@ -0,0 +1,15 @@
+app.js
+// randomuser.me API lekérése
+function getRandomUser() {
+    fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then(data => {
+        const user = data.results[0];
+        document.getElementById('random-name').innerText = '${user.name.first} ${user.name.last}';
+        document.getElementById('random-email').innerText = user.email;
+    })
+    .catch(error => {
+        console.error('Hiba történt :', error);
+    });
+    
+}
